cbf739c3fd544327526f642d3e27f1ff3de5e616
low-streaming <info@low-streaming.de>
2026-05-10 21:55:55 +0200

    et

--- custom_components/openkairo/www/smart_start_screen_card.js
+++ custom_components/openkairo/www/smart_start_screen_card.js
@@ -1,5 +1,5 @@
-// --- OPENKAIRO OS LAUNCHPAD V4.3.0 ---
-console.log("%c 🚀 KAIRO OS V4.3.0 LOADING ", "background: #05f0a0; color: #000; font-weight: bold; padding: 5px;");
+// --- OPENKAIRO OS LAUNCHPAD V4.3.1 ---
+console.log("%c 🚀 KAIRO OS V4.3.1 LOADING ", "background: #05f0a0; color: #000; font-weight: bold; padding: 5px;");
 
 if (!window.openKairoHelpers) {
   window.openKairoHelpers = {
@@ -378,5 +378,5 @@ window.customCards.push({
   type: "openkairo-card",
   name: "OpenKairo OS Launchpad",
   editor: "openkairo-card-editor",
-  description: "Redesigned Bento-Grid OS Layer (V4.2.5)."
+  description: "Redesigned Bento-Grid OS Layer (V4.3.1)."
 });
